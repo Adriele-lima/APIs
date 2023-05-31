da8a0a8c7407fb40a23861281daf76d7707d3b05
Adriele Lima <adriele.p.t@hotmail.com>
2023-05-31 21:32:46 +0000

    Inicial commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const API_KEY = "z6HMOEjzUUnSmpY1y_2Vn_lrjpc";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
+};
